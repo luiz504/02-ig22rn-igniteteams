187fdc456830c447454ec05f9842db94055247fe
Luiz Bueno <luiz504@hotmail.com>
2023-07-31 14:45:55 -0300

    feat: Groups list Components declaration

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,29 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { Container } from './styles'
 
 import { Header } from '@/components/Header'
 import { Highlight } from '@/components/Highlight'
 import { GroupCard } from '@/components/GroupCard'
+import { FlatList } from 'react-native'
 
 export const Groups: FC = () => {
-  const title = 'New Team'
-  const subTitle = 'Create a new Team to add members'
+  const [groups] = useState(['Groups 1', 'Groups 2', 'Groups 3'])
   return (
     <>
-      <Header />
-      <Highlight title={title} subtitle={subTitle} />
-
       <Container testID="wrapper">
-        <GroupCard name="Group Card" />
+        <Header />
+        <Highlight
+          title={'New Team'}
+          subtitle={'Create a new Team to add members'}
+        />
+
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <GroupCard name={item} />}
+          contentContainerStyle={{ rowGap: 12 }}
+        />
       </Container>
     </>
   )
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,15 +1,5 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  background-color: ${({ theme }) => theme.colors['gray-700']};
-  font-size: ${({ theme }) => theme.fontSize.lg};
-  width: 100%;
-  height: 200px;
-  align-items: center;
-  justify-content: center;
-`
-
-export const Text = styled.Text`
-  color: ${({ theme }) => theme.colors['green-500']};
-  font-family: ${({ theme }) => theme.fontFamily.bold};
+  flex: 1;
 `
